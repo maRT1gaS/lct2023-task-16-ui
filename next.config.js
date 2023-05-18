5b3ea762693e38259fa5f07dda48180326bc3ea8
Zuev Dmitry <ZuevDS@mos.ru>
2023-05-18 23:43:21 +0300

    ops: add project config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,56 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      // TODO: поменять hostname на адрес со статикой
+      {
+        protocol: "https",
+        port: "",
+        hostname: "i.ibb.co/**"
+      }
+    ]
+  },
+  webpack(config) {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        use: ["@svgr/webpack"],
+      }
+    );
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+
+  }
 }
 
-module.exports = nextConfig
+const runtimeCaching = require('next-pwa/cache');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  runtimeCaching,
+  disable: process.env.NODE_ENV === 'development' ? true : false,
+});
+
+module.exports = withPWA({
+  ...nextConfig,
+})
