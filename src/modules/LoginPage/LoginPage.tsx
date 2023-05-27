987b3070685837ef354fe34ee18de8551c80684d
Olga <parlelya@vk.com>
2023-05-27 20:12:17 +0300

    Validated login form with yup

--- src/modules/LoginPage/LoginPage.tsx
+++ src/modules/LoginPage/LoginPage.tsx
@@ -2,35 +2,51 @@ import { Form, Field } from 'react-final-form';
 import { Button, Input, Link } from '@/ui';
 import classes from './LoginPage.module.css';
 import type { ILoginFormProps, ILoginValue } from './LoginPage.d';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+	email: Yup.string().email('Invalid email').required('Required'),
+	password: Yup.string().required('Required'),
+});
 
 export const LoginPage = ({ handleOnSubmitForm }: ILoginFormProps) => {
 
 	return (
-		<Form<ILoginValue> onSubmit={handleOnSubmitForm}>
+		<Form<ILoginValue> onSubmit={handleOnSubmitForm} validate={(values) => validationSchema.validateSync(values, { abortEarly: false })}>
 			{(props) => (
 				<form className={classes.LoginForm} onSubmit={props.handleSubmit}>
-					<Field name='email'>
-						{(props) => (
-							<Input
-								labelTitle='Email'
-								className={classes.LoginInput}
-								disabled
-								id='email'
-								{...props.input}
-							/>
-						)}
-					</Field>
-					<Field name='password'>
-						{(props) => (
-							<Input
-								labelTitle='Пароль'
-								className={classes.LoginInput}
-								disabled
-								id='password'
-								{...props.input}
-							/>
-						)}
-					</Field>
+					<div className={classes.LoginInput}>
+						<Field name='email'>
+							{(props) => (
+								<Input
+									labelTitle='Email'
+									className={classes.LoginInput}
+									id='email'
+									{...props.input}
+								/>
+							)}
+						</Field>
+						<Field name="email">
+							{({ meta }) => (meta.error && meta.touched ? <span className={classes.error}>{meta.error}</span> : null)}
+						</Field>
+					</div>
+
+					<div className={classes.LoginInput}>
+						<Field name='password'>
+							{(props) => (
+								<Input
+									labelTitle='Пароль'
+									className={classes.LoginInput}
+									id='password'
+									{...props.input}
+								/>
+							)}
+						</Field>
+						<Field name="password" >
+							{({ meta }) => (meta.error && meta.touched ? <span className={classes.error}>{meta.error}</span> : null)}
+						</Field>
+					</div>
+
 
 					<div className={classes.LoginFormControls}>
 						<Button themeBorder='red' type='submit'>
